d6701059dc5eb53942cbae79d4a21e89bcb50620
MatusSeidl <seidl.matus@gmail.com>
2019-07-28 20:54:27 +0200

    Printing in Color, chalk lib

--- node-app/app.js
+++ node-app/app.js
@@ -1,9 +1,13 @@
 const getNotes = require('./notes.js')
 const validator = require('validator')
 
+const chalk = require('chalk')
+
 
 console.log(getNotes());
 
-
 console.log(validator.isEmail('email@email.com'));
 console.log(validator.isEmail('email@emailcom'));
+
+console.log(chalk.blue('npm install chalk'));
+console.log(chalk.underline.bold.blue('npm install chalk'));
