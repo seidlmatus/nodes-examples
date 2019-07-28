96c421771fd0e8565e816a4ee46a625c43d2e933
MatusSeidl <seidl.matus@gmail.com>
2019-07-28 20:41:39 +0200

    Importing npm Modules

--- node-app/app.js
+++ node-app/app.js
@@ -1,2 +1,9 @@
 const getNotes = require('./notes.js')
+const validator = require('validator')
+
+
 console.log(getNotes());
+
+
+console.log(validator.isEmail('email@email.com'));
+console.log(validator.isEmail('email@emailcom'));
