5c7bb13658e82f8b77177ffef62ecc592cbd7f7a
MatusSeidl <seidl.matus@gmail.com>
2019-07-28 21:06:58 +0200

    nodemon, How to use that...

--- node-app/app.js
+++ node-app/app.js
@@ -10,4 +10,4 @@ console.log(validator.isEmail('email@email.com'));
 console.log(validator.isEmail('email@emailcom'));
 
 console.log(chalk.blue('npm install chalk'));
-console.log(chalk.underline.bold.blue('npm install chalk'));
+console.log(chalk.underline.bold.blue('underline, bold and blue'));
