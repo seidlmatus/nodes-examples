b4758bdb3baf247d43eb92f265095c6cc6225a6b
MatusSeidl <seidl.matus@gmail.com>
2019-07-28 21:18:20 +0200

    Getting input from User

--- node-app/app.js
+++ node-app/app.js
@@ -11,3 +11,14 @@ console.log(validator.isEmail('email@emailcom'));
 
 console.log(chalk.blue('npm install chalk'));
 console.log(chalk.underline.bold.blue('underline, bold and blue'));
+
+
+//read arg
+const command = process.argv[2]
+const title = process.argv[3]
+
+if(command == 'add'){
+    console.log('Adding note! ' + title)
+}else if (command == 'remove'){
+    console.log('Removing note!')
+}
