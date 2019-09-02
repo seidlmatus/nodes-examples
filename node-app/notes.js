5ddab850bfad891941341fb88354fbe6b5e54780
MatusSeidl <seidl.matus@gmail.com>
2019-09-02 16:40:34 +0200

    arrow challenge :)

--- node-app/notes.js
+++ node-app/notes.js
@@ -8,33 +8,29 @@ const getNotes = function () {
 const addNote = function (title, body) {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title;
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
-    if(duplicateNotes.length === 0){
+    if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
         console.log('New note Added')
-    }else {
+    } else {
         console.log("Note title taken!")
     }
 };
 
-const removeNote= function (title) {
+const removeNote = function (title) {
     const notes = loadNotes()
 
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title;
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
-    if(notes.length > notesToKeep.length ){
+    if (notes.length > notesToKeep.length) {
         console.log(chalk.green('Note removed'))
         saveNotes(notesToKeep)
-    }else {
+    } else {
         console.log(chalk.red('No note found!'))
     }
 
@@ -59,5 +55,5 @@ const loadNotes = function () {
 module.exports = {
     addNote: addNote,
     getNotes: getNotes,
-    removeNote:removeNote
+    removeNote: removeNote
 };
