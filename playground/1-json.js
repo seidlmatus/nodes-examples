f0dd64e8985b6a63def1d493b758df5a82b2694f
MatusSeidl <seidl.matus@gmail.com>
2019-09-02 15:27:01 +0200

    read write json to file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,8 +1,16 @@
+const fs = require('fs')
 const book = {
     title: 'Ego is The Enemy',
     author:'Ryan Holiday'
 }
 
-console.log(JSON.stringify(book))
-const parsedData = JSON.parse("{\"title\":\"Ego is The Enemy\",\"author\":\"Ryan Holiday\"}\n")
-console.log(parsedData.author)
+//console.log(JSON.stringify(book))
+//const parsedData = JSON.parse("{\"title\":\"Ego is The Enemy\",\"author\":\"Ryan Holiday\"}\n")
+//console.log(parsedData.author)
+
+//fs.writeFileSync('book.json',JSON.stringify(book));
+
+const dataBuffer = fs.readFileSync('book.json')
+const dataJson = dataBuffer.toString();
+const data = JSON.parse(dataJson);
+console.log(data)
