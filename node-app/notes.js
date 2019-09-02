b882a65134ccc412a6e1ce6249d2733f75b7f198
MatusSeidl <seidl.matus@gmail.com>
2019-09-02 15:49:21 +0200

    save notes to file

--- node-app/notes.js
+++ node-app/notes.js
@@ -1,5 +1,35 @@
-const getNotes = function(){
+const fs = require('fs');
+
+const getNotes = function () {
     return 'My notes...'
+};
+
+const addNode = function (title, body) {
+    const notes = loadNotes()
+    notes.push({
+        title:title,
+        body:body
+    })
+
+    saveNotes(notes)
+};
+
+const saveNotes = function (notes) {
+    const dataJson = JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJson)
 }
 
-module.exports = getNotes
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch (e) {
+        return []
+    }
+};
+
+module.exports = {
+    addNode: addNode,
+    getNotes: getNotes
+};
